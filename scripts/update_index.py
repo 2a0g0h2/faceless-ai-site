128cb52e2ccb362666bead9e5b0d1519b58cf332
2a0g0h2 <2a0g0h2@gmail.com>
2025-08-17 16:16:47 +0200

    Update update_index.py

--- scripts/update_index.py
+++ scripts/update_index.py
@@ -1,59 +1,42 @@
 import os
+from pathlib import Path
 
-POSTS_DIR = "../posts"
-INDEX_FILE = "../index.html"
+POSTS_DIR = "posts"
+INDEX_FILE = "index.html"
 
 def update_index():
-    posts = []
-    for f in sorted(os.listdir(POSTS_DIR), reverse=True):
-        if f.endswith(".html"):
-            with open(os.path.join(POSTS_DIR, f), "r", encoding="utf-8") as file:
-                html = file.read()
-                # Nájde nadpis článku
-                try:
-                    title = html.split("<h1>")[1].split("</h1>")[0]
-                except IndexError:
-                    title = "Neznámy článok"
-                # Nájde obrázok
-                try:
-                    img = html.split('<img src="')[1].split('"')[0]
-                except IndexError:
-                    img = "../images/placeholder.png"
-                posts.append({"title": title, "file": f, "img": img})
+    posts = sorted(Path(POSTS_DIR).glob("*.html"), reverse=True)
     
-    index_html = """
+    index_content = """
     <html>
-    <head>
-        <link rel="stylesheet" href="style.css">
-        <title>Faceless AI Site</title>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Blog</h1>
-            <div class="posts-grid">
+        <head><title>AI Blog Index</title></head>
+        <body>
+            <h1>Latest Posts</h1>
+            <ul>
     """
+
     for post in posts:
-        index_html += f"""
-        <div class="post-card">
-            <a href="posts/{post['file']}">
-                <img src="{post['img']}" alt="{post['title']}">
-                <div class="post-card-content">
-                    <h2>{post['title']}</h2>
-                </div>
-            </a>
-        </div>
+        post_name = post.stem
+        img_path = f"images/{post_name}.png"
+        index_content += f"""
+            <li>
+                <a href="{POSTS_DIR}/{post.name}">
+                    <img src="{img_path}" alt="{post_name}" width="200"><br>
+                    {post_name}
+                </a>
+            </li>
         """
-    index_html += """
-            </div>
-        </div>
-    </body>
+
+    index_content += """
+            </ul>
+        </body>
     </html>
     """
-    
+
     with open(INDEX_FILE, "w", encoding="utf-8") as f:
-        f.write(index_html)
-    
-    print(f"Index aktualizovaný: {INDEX_FILE}")
+        f.write(index_content)
+
+    print("Index updated successfully.")
 
 if __name__ == "__main__":
     update_index()
